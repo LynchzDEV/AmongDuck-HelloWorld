86af03b86157fffbb1aecd5b966a59c110b10ebf
BomScoob <sorawitkraukham@gmail.com>
2023-10-01 16:47:44 +0700

    config export

--- src/main.js
+++ src/main.js
@@ -1,5 +1,27 @@
 import 'phaser';
 import Phaser from 'phaser';
-import config from './gameConfig';
+import _dev from './scenes/_dev';
+
+const config = {
+  // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+  type: Phaser.WEBGL,
+  pixelArt: true,
+  roundPixels: true,
+  parent: 'content',
+  width: 720,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: [_dev],
+};
 
 const game = new Phaser.Game(config);
+
+module.exports = {
+  game,
+  config,
+};
