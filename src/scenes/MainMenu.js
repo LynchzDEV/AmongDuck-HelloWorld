47da18de9924d86ca9974ca4b15d59e98d2e149d
LynchzDEV <opmok48@gmail.com>
2023-10-01 23:25:16 +0700

    Revert "Merge branch 'main' of https://github.com/LynchzDEV/AmoungDuck-HelloWord"
    This reverts commit 9a739114f62f8bd6c3ac2a64f5b032ef40d02ca4, reversing
    changes made to 5f49eacb6dc29bba31784a6656995cd6d4001b2c.

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,9 +1,5 @@
 import Phaser from 'phaser';
-import calculateCanvasRatio from '../utils/ratio';
 
-let canvasWidth;
-let canvasHeight;
-let ratio;
 class MainMenu extends Phaser.Scene {
   constructor() {
     super({
@@ -11,29 +7,9 @@ class MainMenu extends Phaser.Scene {
     });
   }
 
-  preload() {
-    this.load.image('logo', 'src/image/_dev/placeholder.png');
-    this.load.image('button1', 'src/image/_dev/playButton.png');
-    this.load.image('button2', 'src/image/_dev/play.png');
-  }
+  preload() {}
 
-  create() {
-    const canvasData = calculateCanvasRatio(this.sys);
-    canvasWidth = canvasData.canvasWidth;
-    canvasHeight = canvasData.canvasHeight;
-    ratio = canvasData.ratio;
-
-    this.add.image(canvasWidth / 2, canvasHeight / 3.3, 'logo').setScale(0.25);
-
-    const button1 = this.add
-      .image(canvasWidth / 2, canvasHeight / 1.6, 'button1')
-      .setScale(0.5 * ratio)
-      .setInteractive();
-
-    const button2 = this.add
-      .image(canvasWidth / 2, canvasHeight / 1.2, 'button2')
-      .setScale(0.33 * ratio);
-  }
+  create() {}
 
   update(delta, time) {}
 }
