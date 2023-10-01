8f8a2e1078d8c739a1371eafa712899d76ec7d41
LynchzDEV <opmok48@gmail.com>
2023-10-01 22:57:27 +0700

    add files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'phaser';
 import Phaser from 'phaser';
-import _dev from './scenes/_dev';
+import _dev from './utils/_dev';
 import MainMenu from './scenes/MainMenu';
 import CutScene1 from './scenes/CutScene1';
 import CutScene2 from './scenes/CutScene2';
@@ -17,12 +17,12 @@ import Summarize from './scenes/Summarize';
 
 const config = {
   // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
-  type: Phaser.WEBGL,
+  type: Phaser.AUTO,
   pixelArt: true,
   roundPixels: true,
   parent: 'content',
-  width: 720,
-  height: 1080,
+  width: '90vw',
+  height: '90vh',
   physics: {
     default: 'arcade',
     arcade: {
@@ -30,21 +30,21 @@ const config = {
     },
   },
   scene: [
-    MainMenu,
-    CutScene1,
-    CutScene2,
-    Forest1,
-    PathSelection,
-    CodingPath,
-    DesignPath,
-    GameDev,
-    DevOps,
-    FrontEnd,
-    WebDesign,
-    Ending,
-    Summarize,
+    // MainMenu, //* 0 main menu/landing page that have logo and story mode button and endless mode button
+    CutScene1, //* 1 cutscene 1 of the story mode
+    CutScene2, //* 2 cutscene 2 of the story mode
+    Forest1, //* 3 forest map that lead to first decision
+    PathSelection, //* 4 path selection scene
+    CodingPath, //* 5 cutscene that lead to selection of game dev or dev ops
+    DesignPath, //* 6 cutscene that lead to selection of front end or web design
+    GameDev, //* 7 game dev scene: parkour
+    DevOps, //* 8 dev ops scene: delivery man
+    FrontEnd, //* 9 web dev scene: puzzle
+    WebDesign, //* 10 web design scene: adventure
+    Ending, //* 11 show text1
+    Summarize, //* 12 show text2
   ],
-  // scene: [_dev],
+  // scene: [_dev] //! dev mode, comment this line to turn off dev mode
 };
 
 const game = new Phaser.Game(config);
