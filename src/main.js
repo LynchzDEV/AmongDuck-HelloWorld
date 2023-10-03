0c4cc18bd324d33178db9b9b7f299d191f400467
BomScoob <sorawitkraukham@gmail.com>
2023-10-03 15:48:43 +0700

    adjust main

--- src/main.js
+++ src/main.js
@@ -30,22 +30,21 @@ const config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 300 },
       debug: true,
     },
   },
   scene: [
-    //_dev, //! dev mode, comment this line to turn off dev mode
-    //_movementTemplate, //! movement template, comment this line to turn off movement template
+    // _dev, //! dev mode, comment this line to turn off dev mode
+    // _movementTemplate, //! movement template, comment this line to turn off movement template
     // MainMenu, //* 0 main menu/landing page that have logo and story mode button and endless mode button
     // CutScene1, //* 1 cutscene 1 of the story mode
     // CutScene2, //* 2 cutscene 2 of the story mode
-    // Forest1, //* 3 forest map that lead to first decision
-    // PathSelection, //* 4 path selection scene
-    // CodingPath, //* 5 cutscene that lead to selection of game dev or dev ops
-    // DesignPath, //* 6 cutscene that lead to selection of front end or web design
-    // GameDev, //* 7 game dev scene: parkour
-    // DevOps, //* 8 dev ops scene: delivery man
+    Forest1, //* 3 forest map that lead to first decision
+    PathSelection, //* 4 path selection scene
+    CodingPath, //* 5 cutscene that lead to selection of game dev or dev ops
+    DesignPath, //* 6 cutscene that lead to selection of front end or web design
+    GameDev, //* 7 game dev scene: parkour
+    DevOps, //* 8 dev ops scene: delivery man
     FrontEnd, //* 9 web dev scene: puzzle
     WebDesign, //* 10 web design scene: adventure
     Ending, //* 11 show text1
