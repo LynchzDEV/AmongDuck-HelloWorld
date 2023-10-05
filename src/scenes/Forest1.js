5dc384737339380c508037ac538c8c46133bd377
BomScoob <sorawitkraukham@gmail.com>
2023-10-05 13:53:17 +0700

    Revert "Delete forest1.js"
    This reverts commit 8ee3a62f6befce230bde0e9ced498c6865f90e35.

---
+++ src/scenes/Forest1.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser';
+import calculateCanvasRatio from '../utils/ratio';
+import playerMove from '../utils/playerMove';
+
+class Forest1 extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'Forest1',
+    });
+  }
+
+  preload() {
+
+  }
+
+  create() {
+
+  }
+
+  update(delta, time) {
+
+  }
+}
+
+export default Forest1;
