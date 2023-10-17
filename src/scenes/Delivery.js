4d8313dd301fb821ba8e10ef0e8702eba69a7d96
BomScoob <sorawitkraukham@gmail.com>
2023-10-17 17:45:13 +0700

    all done map delivery 1

--- src/scenes/Delivery.js
+++ src/scenes/Delivery.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import path from 'path';
 import {
+  FOREGROUND_TEMPLE_PATH,
   BACKGROUND_GAME_PATH,
   COMPONENT_GAME_PATH,
   PLATFORM_GAME_PATH,
@@ -20,12 +21,14 @@ import {
 import { setWorldBoundsAndCamera } from '../utils/setWorldAndCameraBound';
 
 import playerMoveTemple from '../utils/playerMoveTemple';
+import { OBJECT_SCROLL } from '../utils/mapObjectScroll';
 
 const isMobile = /mobile/i.test(navigator.userAgent);
 const tablet = window.innerWidth < 1280;
 
 //bg component
 let backgrounds;
+let water;
 let cloundLayer1;
 let cloundLayer2;
 let platforms;
@@ -34,9 +37,11 @@ let components;
 let player;
 let camera;
 //interactive
-let chess;
+let milkShop;
 let house;
 let gate;
+let milk1;
+let sign;
 //control flow
 let left;
 let right;
@@ -73,14 +78,6 @@ class Delivery extends Phaser.Scene {
       path.join(PLATFORM_GAME_PATH, 'tile-platform.png')
     );
     this.load.image(
-      'platform-glass',
-      path.join(PLATFORM_GAME_PATH, 'platform-glass.png')
-    );
-    this.load.image(
-      'platform-vine',
-      path.join(PLATFORM_GAME_PATH, 'platform-vine.png')
-    );
-    this.load.image(
       'platform-long1',
       path.join(PLATFORM_GAME_PATH, 'platform-long1.png')
     );
@@ -98,12 +95,24 @@ class Delivery extends Phaser.Scene {
       path.join(PLATFORM_GAME_PATH, 'ground-main.png')
     );
   }
-  loadComponents() {
+  loadMainComponents() {
+    this.load.image(
+      'milk-shop',
+      path.join(COMPONENT_GAME_PATH, 'milk-shop.png')
+    );
+    this.load.image('house', path.join(COMPONENT_GAME_PATH, 'house.png'));
+    this.load.image('milk', path.join(COMPONENT_GAME_PATH, 'milk.png'));
     this.load.image('gate', path.join(COMPONENT_GAME_PATH, 'gate.png'));
     this.load.image(
       'gate-active',
       path.join(COMPONENT_GAME_PATH, 'gate-active.png')
     );
+    this.load.sign = this.load.image(
+      'sign',
+      path.join(COMPONENT_GAME_PATH, 'sign.png')
+    );
+  }
+  loadComponents() {
     this.load.image(
       'sakura-tree',
       path.join(COMPONENT_GAME_PATH, 'Sakura tree.png')
@@ -121,32 +130,24 @@ class Delivery extends Phaser.Scene {
       path.join(COMPONENT_GAME_PATH, 'stone-wall.png')
     );
     this.load.image('stone', path.join(COMPONENT_GAME_PATH, 'stone.png'));
-
-    //load spritesheet
-    //I'm not trying yet, this scale is not correct
-    this.load.spritesheet(
-      'chess',
-      path.join(SPRITESHEET_GAME_PATH, 'chess.png'),
-      {
-        frameWidth: 32,
-        frameHeight: 32,
-      }
+    this.load.image('bench', path.join(COMPONENT_GAME_PATH, 'bench.png'));
+    this.load.image('lantern', path.join(COMPONENT_GAME_PATH, 'lantern.png'));
+    this.load.image('grass', path.join(COMPONENT_GAME_PATH, 'grass.png'));
+    this.load.image('vine', path.join(COMPONENT_GAME_PATH, 'vine.png'));
+  }
+  loadForeground() {
+    this.load.image('water', path.join(FOREGROUND_TEMPLE_PATH, 'Water.png'));
+    this.load.image(
+      'shadow-platform',
+      path.join(COMPONENT_GAME_PATH, 'shadow-short.png')
     );
-    this.load.spritesheet(
-      'flame',
-      path.join(SPRITESHEET_GAME_PATH, 'flame.png'),
-      {
-        frameWidth: 16,
-        frameHeight: 16,
-      }
+    this.load.image(
+      'shadow-platform-long2',
+      path.join(COMPONENT_GAME_PATH, 'shadow-long2.png')
     );
-    this.load.spritesheet(
-      'torch-flame',
-      path.join(SPRITESHEET_GAME_PATH, 'torch-flame.png'),
-      {
-        frameWidth: 16,
-        frameHeight: 16,
-      }
+    this.load.image(
+      'shadow-platform-long1',
+      path.join(COMPONENT_GAME_PATH, 'shadows.png')
     );
   }
   loadPlayer() {
@@ -168,7 +169,9 @@ class Delivery extends Phaser.Scene {
 
   preload() {
     this.loadBackground();
+    this.loadForeground();
     this.loadPlatforms();
+    this.loadMainComponents();
     this.loadComponents();
     this.loadPlayer();
     this.loadUI();
@@ -313,79 +316,121 @@ class Delivery extends Phaser.Scene {
     let bg = this.add
       .tileSprite(0, 0, mapWidth, mapHeight, 'background')
       .setOrigin(0, 0)
-      .setScale(1.5)
-      .setDepth(BACKGROUND_DEPTH)
-      .setScrollFactor(0);
-    //mid clound
-    cloundLayer2 = this.add
-      .tileSprite(0, 0, mapWidth, mapHeight, 'clound-layer2')
-      .setOrigin(0, 0)
-      .setScale(1.5)
-      .setDepth(BACKGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setScale(1.7)
+      .setDepth(SKY_DEPTH)
+      .setScrollFactor(OBJECT_SCROLL.CLOUD - 0.1);
     //front clound
     cloundLayer1 = this.add
+      .tileSprite(0, 0, mapWidth, mapHeight, 'clound-layer2')
+      .setOrigin(0, 0)
+      .setScale(1.7)
+      .setDepth(SKY_DEPTH)
+      .setScrollFactor(OBJECT_SCROLL.CLOUD);
+    // mid clound
+    cloundLayer2 = this.add
       .tileSprite(0, 0, mapWidth, mapHeight, 'clound-layer1')
       .setOrigin(0, 0)
-      .setScale(1.5)
-      .setDepth(BACKGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setScale(1.7)
+      .setDepth(SKY_DEPTH)
+      .setScrollFactor(OBJECT_SCROLL.CLOUD2);
 
     backgrounds.add(bg);
     backgrounds.add(cloundLayer2);
     backgrounds.add(cloundLayer1);
   }
+  //water
+  addForeground(mapWidth, mapHeight) {
+    water = this.add
+      .tileSprite(0, mapHeight - 200, mapWidth, 250, 'water')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setScrollFactor(OBJECT_SCROLL.FG)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+
+    //add shadows
+    this.add
+      .image(1370, mapHeight - 80, 'shadow-platform')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    this.add
+      .image(1530, mapHeight - 110, 'shadow-platform')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    this.add
+      .image(1650, mapHeight - 80, 'shadow-platform-long1')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    this.add
+      .image(2045, mapHeight - 80, 'shadow-platform')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    this.add
+      .image(2380, mapHeight - 110, 'shadow-platform-long2')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    this.add
+      .image(mapWidth-300, mapHeight - 80, 'shadow-platform')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+  }
+  //platforms
   addPlatforms(floorHeight) {
     platforms = this.physics.add.staticGroup();
     let ground = this.add
       .tileSprite(0, floorHeight, 1383, 218, 'ground-main')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformSmall = this.add
       .image(1368, 975, 'platform')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformVine = this.add
-      .image(1537, 778, 'platform-vine')
+      .image(1537, 778, 'platform')
       .setOrigin(0, 0)
       .setScale(1)
       .setSize(222, 74)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformStatue_long = this.add
       .image(1627, 1143, 'platform-long1')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformGlass = this.add
       .image(2035, 967, 'platform')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformHouse = this.add
       .image(2375, 847, 'platform-long2')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformGate = this.add
       .image(3549, 872, 'platform')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     let platformSakuraTree = this.add
       .image(929, 695, 'platform-long1')
       .setOrigin(0, 0)
       .setScale(1)
-      .setDepth(MIDDLEGROUND_DEPTH)
-      .setScrollFactor(0);
+      .setDepth(MIDDLEGROUND_DEPTH);
+
     platforms.add(ground);
     platforms.add(platformSmall);
     platforms.add(platformVine);
@@ -395,16 +440,110 @@ class Delivery extends Phaser.Scene {
     platforms.add(platformGate);
     platforms.add(platformSakuraTree);
   }
-  addPlayerAndColider(floorHeight){
+  //house, milk shop, milk, gate, sign
+  addMainComponents() {
+    components = this.add.group();
+    milkShop = this.add
+      .image(574, 884, 'milk-shop')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    house = this.add
+      .image(2620, 400, 'house')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH);
+    milk1 = this.add
+      .image(1102, 584, 'milk')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(MIDDLEGROUND_DEPTH);
+    gate = this.add
+      .image(3650, 787, 'gate')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(MIDDLEGROUND_DEPTH);
+    gate.flipX = true;
+    sign = this.add
+      .image(2447, 701, 'sign')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(MIDDLEGROUND_DEPTH);
+
+    components.add(milkShop);
+    components.add(house);
+    components.add(milk1);
+    components.add(gate);
+    components.add(sign);
+  }
+  //prop
+  addComponents() {
+    //sakura milk shop
+    this.add
+      .image(141, 612, 'sakura-tree')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1).flipX = true;
+    this.add
+      .image(700, 266, 'sakura-tree')
+      .setOrigin(0, 0)
+      .setScale(0.7)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+    this.add
+      .image(1672, 1049, 'stone-wall')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+    this.add
+      .image(1949, 967, 'statue-stone')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+    this.add
+      .image(1075, 589, 'bench')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+    this.add
+      .image(1546, 725, 'stone')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+    this.add
+      .image(1391, 853, 'lantern')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+    //add vine on platformVine
+    this.add
+      .image(1620, 772, 'vine')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(MIDDLEGROUND_DEPTH + 1);
+    this.add
+      .image(2178, 940, 'grass')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(PLAYER_DEPTH + 1);
+    //sakura house
+    this.add
+      .image(2400, 238, 'sakura-tree')
+      .setOrigin(0, 0)
+      .setScale(1)
+      .setDepth(BACKGROUND_COMPONENT_DEPTH - 1);
+  }
+  //player and colider
+  addPlayerAndColider(floorHeight) {
     //player
     player = this.physics.add
-      .sprite(100,floorHeight-200, 'player')
+      .sprite(100, floorHeight - 200, 'player')
       .setCollideWorldBounds(true)
       .setScale(3)
       .setDepth(PLAYER_DEPTH);
 
     this.physics.add.collider(player, platforms);
   }
+  //animations
   addAnimations() {
     //animations for testing
     this.anims.create({
@@ -417,27 +556,27 @@ class Delivery extends Phaser.Scene {
       repeat: -1,
     });
 
-    //water animation
-    this.anims.create({
-      key: 'waterAnim',
-      frames: this.anims.generateFrameNumbers('water', {
-        start: 0,
-        end: 5,
-      }),
-      frameRate: 5.5,
-      repeat: -1,
-    });
+    // //water animation
+    // this.anims.create({
+    //   key: 'waterAnim',
+    //   frames: this.anims.generateFrameNumbers('water', {
+    //     start: 0,
+    //     end: 5,
+    //   }),
+    //   frameRate: 5.5,
+    //   repeat: -1,
+    // });
 
-    //sakura animation
-    this.anims.create({
-      key: 'sakura',
-      frames: this.anims.generateFrameNumbers('sakura', {
-        start: 0,
-        end: 20,
-      }),
-      frameRate: 8,
-      repeat: -1,
-    });
+    // //sakura animation
+    // this.anims.create({
+    //   key: 'sakura',
+    //   frames: this.anims.generateFrameNumbers('sakura', {
+    //     start: 0,
+    //     end: 20,
+    //   }),
+    //   frameRate: 8,
+    //   repeat: -1,
+    // });
   }
   create() {
     //config
@@ -450,7 +589,7 @@ class Delivery extends Phaser.Scene {
     const mapWidth = width;
     const mapHeight = height;
 
-    const floorHeight = height - 215;
+    const floorHeight = mapHeight - 215;
 
     //binding function
     this.playerMoveTemple = playerMoveTemple;
@@ -469,8 +608,14 @@ class Delivery extends Phaser.Scene {
     this.addAnimations();
     //background
     this.addBackgroundElements(mapWidth, mapHeight);
+    //foreground
+    this.addForeground(mapWidth, mapHeight);
     //platforms
     this.addPlatforms(floorHeight);
+    //main components
+    this.addMainComponents();
+    //components pragob
+    this.addComponents();
     //player and colider
     this.addPlayerAndColider(floorHeight);
   }
