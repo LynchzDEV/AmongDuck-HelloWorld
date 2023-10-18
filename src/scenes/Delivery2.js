9a55768c6829c53046a58ce522e49fdf84581fe8
BomScoob <sorawitkraukham@gmail.com>
2023-10-18 13:49:17 +0700

    setting background on delivery 2

--- src/scenes/Delivery2.js
+++ src/scenes/Delivery2.js
@@ -1,4 +1,21 @@
 import Phaser from "phaser";
+import playerMoveTemple from "../utils/playerMoveTemple";
+import setWorldBoundsAndCamera from "../utils/setWorldAndCameraBound";
+import path from "path";
+import {
+  BACKGROUND_GAME_PATH,
+  PLATFORM_GAME_PATH,
+} from "../utils/mapPath";
+import {
+  SKY_DEPTH,
+} from "../utils/mapDepth";
+import {
+  OBJECT_SCROLL,
+} from "../utils/mapObjectScroll";
+
+let backgrounds
+let cloundLayer1
+let cloundLayer2
 
 class Delivery2 extends Phaser.Scene {
   constructor() {
@@ -6,10 +23,97 @@ class Delivery2 extends Phaser.Scene {
       key: "Delivery2",
     });
   }
+  loadBackground() {
+    this.load.image(
+      'background',
+      path.join(BACKGROUND_GAME_PATH, 'background-pink.png')
+    );
+    this.load.image(
+      'clound-layer1',
+      path.join(BACKGROUND_GAME_PATH, 'bg-pink-layer1.png')
+    );
+    this.load.image(
+      'clound-layer2',
+      path.join(BACKGROUND_GAME_PATH, 'bg-pink-layer2.png')
+    );
+  }
+  loadPlatforms() {
+    this.load.image('platform', path.join(PLATFORM_GAME_PATH, 'platform.png'));
+    this.load.image(
+      'tile-platfrom',
+      path.join(PLATFORM_GAME_PATH, 'tile-platform.png')
+    );
+    this.load.image(
+      'platform-long1',
+      path.join(PLATFORM_GAME_PATH, 'platform-long1.png')
+    );
+    this.load.image(
+      'platform-long2',
+      path.join(PLATFORM_GAME_PATH, 'platform-long2.png')
+    );
+    this.load.image('ground', path.join(PLATFORM_GAME_PATH, 'ground.png'));
+    this.load.image(
+      'ground-edge',
+      path.join(PLATFORM_GAME_PATH, 'ground-edge.png')
+    );
+    this.load.image(
+      'ground-main',
+      path.join(PLATFORM_GAME_PATH, 'ground-main.png')
+    );
+  }
 
-  preload() {}
+  preload() {
+    this.loadBackground();
+    this.loadPlatforms();
+  }
 
-  create() {}
+  addBackgroundElements(mapWidth, mapHeight) {
+    backgrounds = this.add.group();
+    let bg = this.add
+      .tileSprite(0, 0, mapWidth, mapHeight, 'background')
+      .setOrigin(0, 0)
+      .setScale(1.6)
+      .setDepth(SKY_DEPTH)
+      .setScrollFactor(OBJECT_SCROLL.CLOUD - 0.1);
+    //mid clound
+    cloundLayer1 = this.add
+      .tileSprite(0, 0, mapWidth, mapHeight, 'clound-layer2')
+      .setOrigin(0, 0)
+      .setScale(1.6)
+      .setDepth(SKY_DEPTH)
+      .setScrollFactor(OBJECT_SCROLL.CLOUD);
+    // front
+    cloundLayer2 = this.add
+      .tileSprite(0, 0, mapWidth, mapHeight, 'clound-layer1')
+      .setOrigin(0, 0)
+      .setScale(1.6)
+      .setDepth(SKY_DEPTH)
+      .setScrollFactor(OBJECT_SCROLL.CLOUD2);
+
+    backgrounds.add(bg);
+    backgrounds.add(cloundLayer2);
+    backgrounds.add(cloundLayer1);
+  }
+
+  create() {
+    //config
+    const { width, height } = this.scale;
+    // main scale
+    const mapWidth = width * 3;
+    const mapHeight = height * 2;
+
+    //Dev scale 3840 * 1440
+    // const mapWidth = width;
+    // const mapHeight = height;
+
+    const floorHeight = mapHeight - 215;
+
+    //binding function
+    this.playerMoveTemple = playerMoveTemple;
+    this.setWorldBoundsAndCamera = setWorldBoundsAndCamera;
+
+    this.addBackgroundElements(mapWidth, mapHeight);
+  }
 
   update(delta, time) {}
 }
